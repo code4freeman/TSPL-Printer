f5a4600fb91ceea5a197b5c2a53aeb38b00b99f5
lilindog <lilin@lilin.site>
2022-05-28 23:10:29 +0800

    继续封装crc

--- utils/Crc.class.js
+++ utils/Crc.class.js
@@ -1,11 +1,15 @@
 class Crc {
     static CrcTypes = {
         crc32: {
+            _crcWidth: 32,
             poly: "",
             init: 0xffffffff,
             refin: true,
             refout: true,
-            xorout: true
+            xorout: true,
+            table: [
+
+            ]
         }
     }
     static BinLength (num = 0x00) {
@@ -16,14 +20,13 @@ class Crc {
         }
         return len;
     }
-    static revesebin (value = 0x00) {
+    static ReveseBin (value = 0x00) {
         let out = 0, i;
         for (i = 0; i < Crc.binLength(value); i++) {
             out <<= 1;
             out |= value >> i & 0x01;
         }
         return out;
-
     }
     type = null;
     constructor (type) {
@@ -32,24 +35,49 @@ class Crc {
         }
         this.type = Crc.CrcTypes[type];
     }
-    build (data) {
 
+    /**
+     * 计算crc
+     *
+     * @param {Uint8Array} data
+     * @return {Uint8Array}
+     * @public
+     */
+    calc (data) {
+        const option = Crc.CrcTypes[this.type];
+        let crc = options.init;
+        for (let i = 0; i < Uint8Array.byteLength; i++) {
+            if (option.refin) {
+                crc ^= data[i];
+            } else {
+                crc ^= data[i] << option._crcWidth - 8;
+            }
+            crc ^= data[i];
+            for (let j = 0; j < 8; j++) {
+                if (options.refin) {
+                    crc & 0x01 ?
+                    crc = crc >>> 1 ^ options.poly :
+                    crc >>>= 1;
+                } else {
+                    crc & 0x01 <<
+                }
+                if (crc & 0x01) {
+                    crc = crc >>> 1 ^ option.poly;
+                } else {
+                    crc >>>= 1;
+                }
+            }
+        }
     }
+
+    /**
+     * 验证crc
+     *
+     * @param {Uint8Array} data
+     * @returns {Boolean}
+     * @public
+     */
     verify (data) {
-
+        return Crc.CrcTypes[this.type].verify(data);
     }
 }
-
-function getBinNumber (num = 0x00) {
-    let count = 0;
-    let str = "";
-    while (num) {
-        str = num % 2 + str;
-        num = num / 2 | 0;
-        count++;
-    }
-    return [ count, str ];
-}
-
-let v = 32;
-console.log((v).toString(2).length, getBinNumber(v));
